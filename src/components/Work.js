08bf3ed6648ae3132c8efe23b464f4c342e087b1
Tyler Green <cardsfan2019@gmail.com>
2023-03-09 00:58:36 -0600

    work descriptions

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,10 +8,18 @@ export default function Work() {
                     </div>
                     <div className="work-text">
                         Starting during the Summer of 2020, I have been an intern at John Deere.
-                        My internships consisted of
-                        data analytics (2020),
-                        fronted/backend web development (2021),
-                        and embedded software verification (2022).
+                        My internships consisted of:
+                        <ul>
+                            <li>Data analytics (2020)</li>
+                            <li>Fronted/backend web development (2021)</li>
+                            <li>Embedded software verification (2022)</li>
+                        </ul>
+                        During my virtual and in-the-office internships, my experiences spanned many
+                        different fields (literally) of engineering. I gained hands on experience with
+                        hardware testing, as well as in simulation. I also helped develop tools to make employees
+                        jobs easier, including a financial data analysis tool and Rally data export tool.
+                        During my summer of 2022, I was able to get my first experience in the office since
+                        being virtual, and I am grateful that I got that before beginning a career.
                     </div>
                 </div>
                 <div className="work-right">
@@ -28,23 +36,25 @@ export default function Work() {
                         I was able to take part in the ARA Wireless program.
                         ARA is:
                         <blockquote cite="https://arawireless.org/about-ara/">
-                        An at-scale platform for advanced wireless research
-                        to be deployed across the Iowa State University (ISU) campus,
-                        City of Ames (where ISU resides), and surrounding research and producer
-                        farms as well as rural communities in central Iowa, spanning a rural area
-                        with diameter over 60km. It serves as a wireless living lab for smart and
-                        connected rural communities, enabling the research and development of
-                        rural-focused wireless technologies that provide affordable, high-capacity
-                        connectivity to rural communities and industries such as agriculture.
+                            An at-scale platform for advanced wireless research
+                            to be deployed across the Iowa State University (ISU) campus,
+                            City of Ames (where ISU resides), and surrounding research and producer
+                            farms as well as rural communities in central Iowa, spanning a rural area
+                            with diameter over 60km. It serves as a wireless living lab for smart and
+                            connected rural communities, enabling the research and development of
+                            rural-focused wireless technologies that provide affordable, high-capacity
+                            connectivity to rural communities and industries such as agriculture.
                         </blockquote>
                         My role in the ARA program was to develop a deployment/development strategy
                         for OpenStack, an open-source cloud platform. During the assistantship, I
                         gained experience working in GitLab CI/CD, a system for building, testing,
-                        and deploying software automatically.
+                        and deploying software automatically. I also learned a significant amount about
+                        OpenStack, and took part in some of the development work that the rest of my team
+                        was mostly responsible for.
                     </div>
                 </div>
                 <div className="work-right">
-                    <img className="work-right-image" alt="ARA program logo" src="https://www.news.iastate.edu/media/2021/06/WF0J.jpg"></img>
+                    <img className="work-right-image" alt="ARA program logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Farawireless.org%2Fwp-content%2Fuploads%2F2021%2F05%2FARA_logo4-01-1.png&f=1&nofb=1&ipt=14243ba6e54c33e9fe07210652b6ad0eb924b3ee71bff438728d3bd571a05db3&ipo=images"></img>
                 </div>
             </div>
         </div>
