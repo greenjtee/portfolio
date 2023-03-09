8270fea240456c325591b18983f0595bd70a86f3
Tyler Green <cardsfan2019@gmail.com>
2023-03-09 00:31:16 -0600

    start of work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,36 @@
 export default function Work() {
     return (
-        <h2>Work Experience</h2>
+        <div className="work">
+            <div className="work-item">
+                <div className="work-left">
+                    <div className="work-header-text">
+                        John Deere
+                    </div>
+                    <div className="work-text">
+                        Starting during the Summer of 2020, I have been an intern at John Deere.
+                        My internships consisted of
+                        data analytics (2020),
+                        fronted/backend web development (2021),
+                        and embedded software verification (2022).
+                    </div>
+                </div>
+                <div className="work-right">
+                    <img className="work-right-image" alt="John Deere logo" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fc%2Fcf%2FJohn_Deere_logo.svg%2F1280px-John_Deere_logo.svg.png&f=1&nofb=1&ipt=259dcd418adb9bcebd42ad919c5e3bf7b502a08e22d79c2050f156aa002b87b7&ipo=images"></img>
+                </div>
+            </div>
+            <div className="work-item">
+                <div className="work-left">
+                    <div className="work-header-text">
+                        ARA (undergraduate research assistant)
+                    </div>
+                    <div className="work-text">
+                        My role in the ARA program
+                    </div>
+                </div>
+                <div className="work-right">
+                    <img className="work-right-image" alt="ARA program logo" src="https://www.news.iastate.edu/media/2021/06/WF0J.jpg"></img>
+                </div>
+            </div>
+        </div>
     );
 }
