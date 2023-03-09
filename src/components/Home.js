2fa3f62c92d14c40e909c35f8592036bb7331d15
Tyler Green <cardsfan2019@gmail.com>
2023-03-08 23:48:05 -0600

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
+import marathon_photo from '../images/half-marathon.JPG'
+
 export default function Home() {
    return (
       <div className="home">
-         <div className="home-text">
-            <h2>
+         <div className="home-left">
+            <div className="home-text">
                I am a Senior studying Computer Engineering.
-               My expertise is in software develoment and embedded systems.
+               My expertise is in <strong>software develoment</strong> and <strong>embedded systems</strong>.
                Some of my hobbies outside of class are:
                <ul>
                   <li>Running</li>
@@ -12,12 +14,15 @@ export default function Home() {
                   <li>Video games (Rocket League)</li>
                   <li>Programming</li>
                </ul>
-            </h2 >
+            </div>
+            <div className="home-images">
+               <img alt="Texas Independence Relay - Come and Take It" className="home-image" src="https://lonestarrelays.com/wp-content/uploads/2021/04/TIR_Logo_MHLightOnDark-1-1-2048x2015.png"></img>
+               <img alt="Rocket League shield" className="home-image" src="https://sn9jpk.media.zestyio.com/Shield-Solid-On-Dark.png"></img>
+               <img alt="Visual studio code logo" className="home-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuser-images.githubusercontent.com%2F674621%2F71187801-14e60a80-2280-11ea-94c9-e56576f76baf.png&f=1&nofb=1&ipt=be92305ee18db7706e4b38de96dd20002783fd2b3dd8646ba28f8868bb530e62&ipo=images"></img>
+            </div>
          </div>
-         <div className="home-images">
-            <img alt="Texas Independence Relay - Come and Take It" className="home-image" src="https://lonestarrelays.com/wp-content/uploads/2021/04/TIR_Logo_MHLightOnDark-1-1-2048x2015.png"></img>
-            <img alt="Rocket League shield" className="home-image" src="https://sn9jpk.media.zestyio.com/Shield-Solid-On-Dark.png"></img>
-            <img alt="Visual studio code logo" className="home-image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuser-images.githubusercontent.com%2F674621%2F71187801-14e60a80-2280-11ea-94c9-e56576f76baf.png&f=1&nofb=1&ipt=be92305ee18db7706e4b38de96dd20002783fd2b3dd8646ba28f8868bb530e62&ipo=images"></img>
+         <div className="home-right">
+            <img alt="Aerial view of marathon runners" className="home-half-image" src={marathon_photo}></img>
          </div>
       </div>
    );
