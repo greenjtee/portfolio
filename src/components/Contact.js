53859efa4d64d1bfbd335045c8ce6a9e05d442e6
Tyler Green <cardsfan2019@gmail.com>
2023-03-15 13:29:14 -0500

    contact page done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,13 @@
 export default function Contact() {
    return (
-      <h2>Contact</h2>
+      <div style={{"padding": "30px"}}>
+         <h2>Contact</h2>
+         <h3>
+            Email: <a href="mailto:greenjtee@gmail.com">greenjtee@gmail.com</a>
+         </h3>
+         <h3>
+            Phone: 1-(309)-714-3010
+         </h3>
+      </div>
    );
 }
